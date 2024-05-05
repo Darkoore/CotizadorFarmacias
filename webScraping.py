da8201db7d843bca234b39e211b4c45e941a6fb0
Darkoore <80989378+Darkoore@users.noreply.github.com>
2024-05-05 19:13:47 -0400

    Formato de strings y creacion de csv
    Se puede modificar la escabilidad del scrapeo cambiando al sz de sitioObjetivo

--- webScraping.py
+++ webScraping.py
@@ -1,10 +1,8 @@
-<<<<<<< Updated upstream
-=======
 from bs4 import BeautifulSoup
 import requests
-
-# URL del sitio a scrapear
-sitioObjetivo = 'https://www.farmaciasahumada.cl/medicamentos'
+import pandas as pd
+# URL del sitio a scrapear, cambiar el valor de sz(max 2800) para aumentar el rango del scraping
+sitioObjetivo = 'https://www.farmaciasahumada.cl/on/demandware.store/Sites-ahumada-cl-Site/default/Search-UpdateGrid?cgid=medicamentos&start=0&sz=50'
 
 # Hacer una solicitud GET al sitio
 resultado = requests.get(sitioObjetivo)
@@ -12,36 +10,50 @@ resultado = requests.get(sitioObjetivo)
 # Parsear el contenido HTML de la página
 cargarPagina = BeautifulSoup(resultado.content, "lxml")
 
-# Buscar el contenedor principal donde se encuentran los medicamentos
-tablasContenidos = cargarPagina.find('div', attrs={'class':"col-sm-12 col-md-9"})
-if not tablasContenidos:
-    print("No se encontró el contenedor principal de los medicamentos.")
-
 # Listas para almacenar la información recopilada
-nombres = []
+laboratorios = []
 precios = []
 dosises = []
 links = []
-
+data = []
+ids = []
+id= 0
 # Iterar sobre cada elemento que contiene información de un medicamento
-if tablasContenidos:
-    for contenido in tablasContenidos.find_all('div', attrs={'class':"col-6 col-sm-4 col-lg-3"}):
-        nombre = contenido.find('span', attrs={'class':"product-tile-brand"})
-        precio = contenido.find('span', attrs={'class':"value d-flex align-items-center"})
+if cargarPagina:
+    for contenido in cargarPagina.find_all('div', attrs={'class':"col-6 col-sm-4 col-lg-3"}):
+        laboratorio = contenido.find('span', attrs={'class':"link"})
+        precio = contenido.find('span', attrs={'class':"value d-flex align-items-center"}).text.strip()
         dosis = contenido.find('a', attrs={'class':"link"})
         link = contenido.find('a', attrs={'class':"link"})
-        
+        id += 1
         # Añadir los resultados a las listas correspondientes
-        nombres.append(nombre.text if nombre else "No disponible")
-        precios.append(precio.text if precio else "No disponible")
+        ids.append(id)
+        laboratorios.append(laboratorio.text if laboratorio else "No disponible")
+        precios.append(precio if precio else "No disponible")
         dosises.append(dosis.text if dosis else "No disponible")
-        links.append(link.get("href") if link else "No disponible")
+        links.append("https://www.farmaciasahumada.cl"+link.get("href") if link else "No disponible")
 
-    # Mostrar los resultados
-    for i in range(len(nombres)):
-        print(f"Nombre: {nombres[i]}, Precio: {precios[i]}, Dosis: {dosises[i]}, Link: {links[i]}")
+    # Mostrar los resultados y almacenamiento de datos en un csv
+    id = 0
+    for i in range(len(laboratorios)):
+       
+        id += 1
+        print("\n---------------------------------------------------------------------",f"\nID: {id}\nNombre: {laboratorios[i]} \nPrecio: {precios[i]} \nDosis: {dosises[i]} \nLink: {links[i]}")
+        
+        # Creacion de dataframe para generar archivo csv para pandas
+        catastro = {
+            'ID' : ids,
+            'Laboratorio': laboratorios,
+            'Precio': precios,
+            'Dosis': dosises,
+            'Link': links
+        }
+        dataFrame = pd.DataFrame(catastro)
+
+        # Guardar el DataFrame en un archivo CSV
+        dataFrame.to_csv('catalagoAhumada.csv', index=False)
 else:
     print("No se encontraron medicamentos.")
 
 
->>>>>>> Stashed changes
+
